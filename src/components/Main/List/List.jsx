b82c1f4bbff15d7cc047aff4bf594e9b57a2d270
Georgey <georgey.gerasimov@gmail.com>
2023-12-26 01:20:01 +0300

    add dropdown

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,17 +2,55 @@ import style from './List.module.css';
 import {Post} from './Post/Post';
 
 export const List = () => {
-  const postData = {
-    thumbnail: '',
-    title: 'Title',
-    author: 'Nickname',
-    ups: 50,
-    date: '2023-12-25T20:04:00.000Z'
-  };
+  const postsData = [
+    {
+      id: 123,
+      thumbnail: '',
+      title: 'Title111',
+      author: 'Nickname1',
+      ups: 55,
+      date: '2023-12-25T20:04:00.000Z'
+    },
+    {
+      id: 456,
+      thumbnail: '',
+      title: 'Title222',
+      author: 'Nickname2',
+      ups: 66,
+      date: '2023-12-25T20:04:00.000Z'
+    },
+    {
+      id: 789,
+      thumbnail: '',
+      title: 'Title333',
+      author: 'Nickname3',
+      ups: 77,
+      date: '2023-12-25T20:04:00.000Z'
+    },
+    {
+      id: 1011,
+      thumbnail: '',
+      title: 'Title444',
+      author: 'Nickname4',
+      ups: 88,
+      date: '2023-12-25T20:04:00.000Z'
+    },
+    {
+      id: 1213,
+      thumbnail: '',
+      title: 'Title555',
+      author: 'Nickname5',
+      ups: 99,
+      date: '2023-12-25T20:04:00.000Z'
+    }
+  ];
 
   return (
     <ul className={style.list}>
-      <Post postData={postData} />
+      {postsData.map(postData =>
+        <Post key={postData.id} postData={postData} />
+      )}
+
     </ul>
   );
 };
--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,8 +1,84 @@
+import {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+
 import style from './Tabs.module.css';
 
-export const Tabs = () => <ul className={style.list}>
-  <li><a href='/'>Главная</a></li>
-  <li><a href='/'>Просмотренные</a></li>
-  <li><a href='/'>Сохраненные</a></li>
-  <li><a href='/'>Мои посты</a></li>
-</ul>;
+import {assignId} from '../../../utils/generateRandomId';
+import {debounceRaf} from '../../../utils/debounce';
+
+import {ReactComponent as ArrowIcon} from './img/arrow.svg';
+import {ReactComponent as HomeIcon} from './img/home.svg';
+import {ReactComponent as TopIcon} from './img/top.svg';
+import {ReactComponent as BestIcon} from './img/best.svg';
+import {ReactComponent as Hot} from './img/hot.svg';
+
+const LIST = [
+  {value: 'Главная', Icon: HomeIcon},
+  {value: 'Топ', Icon: TopIcon},
+  {value: 'Лучшие', Icon: BestIcon},
+  {value: 'Горячие', Icon: Hot},
+].map(assignId);
+
+export const Tabs = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdown, setIsDropdown] = useState(true);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleRasize = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setIsDropdown(true);
+    } else {
+      setIsDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    const debounceResize = debounceRaf(handleRasize);
+    debounceResize();
+
+    window.addEventListener('resize', debounceResize);
+
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <div className={style.container}>
+      {isDropdown && (
+        <div className={style.wrapperBtn}>
+          <button
+            className={style.btn}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            {selectedItem ? selectedItem.value : 'Главная'}
+            <ArrowIcon width={15} height={15} />
+          </button>
+        </div>
+      )}
+
+      {(isDropdownOpen || !isDropdown) &&
+        <ul className={style.list} onClick={() => setIsDropdownOpen(false)}>
+          {LIST.map(({value, id, Icon}) => (
+            <li className={style.item} key={id}>
+              <button
+                className={style.btn}
+                onClick={() => handleItemClick({value, id, Icon})}>
+                {value}
+                {Icon && <Icon width={30} height={30}/>}
+              </button>
+            </li>
+          ))}
+        </ul>
+      }
+    </div>
+  );
+};
+
+Tabs.propTypes = {
+  list: PropTypes.array
+};
